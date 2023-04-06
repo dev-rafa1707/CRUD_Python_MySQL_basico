f19214e13ce76d5a34c0e3a3edcb7a5d2286def7
dev-rafa1707 <rafa1707@gmail.com>
2023-04-06 10:51:18 -0300

    add funcionalidade UPDATE ao app.py

--- app.py
+++ app.py
@@ -21,11 +21,25 @@ cursor = conexao.cursor()
 # conexao.commit() 
 
 #READ ALL
-comando = f'SELECT * FROM vendas'
-cursor.execute(comando)
-resultado = cursor.fetchall() 
-print(resultado)
+# comando = f'SELECT * FROM vendas'
+# cursor.execute(comando)
+# resultado = cursor.fetchall() 
+# print(resultado)
 
+
+#READ BY ID
+# idVenda = 4
+# comando = f'SELECT * FROM vendas WHERE idVenda = {idVenda}'
+# cursor.execute(comando)
+# resultado = cursor.fetchall()
+# print(resultado)
+
+#UPDATE
+idVenda = 4
+valor = 79
+comando = f'UPDATE vendas SET valor = {valor} WHERE idVenda = {idVenda}'
+cursor.execute(comando)
+conexao.commit()
 
 
 
