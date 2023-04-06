930b26696c2521d959957f475388e98bfdb5ca38
dev-rafa1707 <rafa1707@gmail.com>
2023-04-06 10:55:52 -0300

    add funcionalidade DELETE

--- app.py
+++ app.py
@@ -14,25 +14,25 @@ cursor = conexao.cursor()
 # CRUD
 
 # CREATE
-# nomeProduto ='Mouse optico' #Informar o nome do produto
-# valor=99 #Informar o valor
-# comando = f'INSERT INTO vendas (nomeProduto, valor) VALUES ("{nomeProduto}",{valor})'
-# cursor.execute(comando)
-# conexao.commit() 
+nomeProduto ='Mouse optico' #Informar o nome do produto
+valor=99 #Informar o valor
+comando = f'INSERT INTO vendas (nomeProduto, valor) VALUES ("{nomeProduto}",{valor})'
+cursor.execute(comando)
+conexao.commit() 
 
 #READ ALL
-# comando = f'SELECT * FROM vendas'
-# cursor.execute(comando)
-# resultado = cursor.fetchall() 
-# print(resultado)
+comando = f'SELECT * FROM vendas'
+cursor.execute(comando)
+resultado = cursor.fetchall() 
+print(resultado)
 
 
 #READ BY ID
-# idVenda = 4
-# comando = f'SELECT * FROM vendas WHERE idVenda = {idVenda}'
-# cursor.execute(comando)
-# resultado = cursor.fetchall()
-# print(resultado)
+idVenda = 4
+comando = f'SELECT * FROM vendas WHERE idVenda = {idVenda}'
+cursor.execute(comando)
+resultado = cursor.fetchall()
+print(resultado)
 
 #UPDATE
 idVenda = 4
@@ -41,6 +41,11 @@ comando = f'UPDATE vendas SET valor = {valor} WHERE idVenda = {idVenda}'
 cursor.execute(comando)
 conexao.commit()
 
+#DELETE
+idVenda = 2
+comando = f'DELETE FROM vendas WHERE idVenda = {idVenda}'
+cursor.execute(comando)
+conexao.commit()
 
 
 
